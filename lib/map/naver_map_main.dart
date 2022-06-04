eaa00b4462df6ea1ad0ef6dcb40e186618ec6681
tranzal <64002448+tranzal@users.noreply.github.com>
2022-06-05 00:46:14 +0900

    좌표 구하는 방법 추가

--- lib/map/naver_map_main.dart
+++ lib/map/naver_map_main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:naver_map_plugin/naver_map_plugin.dart';
 
 // https://api.ncloud-docs.com/docs/ai-naver-mapsgeocoding-geocode 참고
@@ -16,25 +18,223 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  static const MODE_ADD = 0xF1;
+  static const MODE_REMOVE = 0xF2;
+  static const MODE_NONE = 0xF3;
+  int _currentMode = MODE_NONE;
 
-  final scaffoldKey = GlobalKey<ScaffoldState>();
   Completer<NaverMapController> _controller = Completer();
+  List<Marker> _markers = [];
+
+  @override
+  void initState() {
+    // WidgetsBinding.instance.addPostFrameCallback((_) {
+    //   OverlayImage.fromAssetImage(
+    //     assetName: 'icon/marker.png',
+    //   ).then((image) {
+    //     setState(() {
+    //       _markers.add(Marker(
+    //           markerId: 'id',
+    //           position: const LatLng(37.563600, 126.962370),
+    //           captionText: "커스텀 아이콘",
+    //           captionColor: Colors.indigo,
+    //           captionTextSize: 20.0,
+    //           alpha: 0.8,
+    //           captionOffset: 30,
+    //           icon: image,
+    //           anchor: AnchorPoint(0.5, 1),
+    //           width: 45,
+    //           height: 45,
+    //           infoWindow: '인포 윈도우',
+    //           onMarkerTab: _onMarkerTap));
+    //     });
+    //   });
+    // });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: NaverMap(
-          onMapCreated: _onMapCreated,
-
+      home: SafeArea(
+        bottom: true,
+        top: true,
+        left: true,
+        right: true,
+        child: Scaffold(
+          appBar: AppBar(),
+          body: Column(
+            children: <Widget>[
+              _controlPanel(),
+              _naverMap(),
+            ],
+          ),
         ),
       ),
     );
   }
 
+  _controlPanel() {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          // 추가
+          Expanded(
+            child: GestureDetector(
+              onTap: () => setState(() => _currentMode = MODE_ADD),
+              child: Container(
+                decoration: BoxDecoration(
+                    color:
+                    _currentMode == MODE_ADD ? Colors.black : Colors.white,
+                    borderRadius: BorderRadius.circular(6),
+                    border: Border.all(color: Colors.black)),
+                padding: const EdgeInsets.all(8),
+                margin: const EdgeInsets.only(right: 8),
+                child: Text(
+                  '추가',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color:
+                    _currentMode == MODE_ADD ? Colors.white : Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // 삭제
+          Expanded(
+            child: GestureDetector(
+              onTap: () => setState(() => _currentMode = MODE_REMOVE),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: _currentMode == MODE_REMOVE
+                        ? Colors.black
+                        : Colors.white,
+                    borderRadius: BorderRadius.circular(6),
+                    border: Border.all(color: Colors.black)),
+                padding: const EdgeInsets.all(8),
+                margin: const EdgeInsets.only(right: 8),
+                child: Text(
+                  '삭제',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: _currentMode == MODE_REMOVE
+                        ? Colors.white
+                        : Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          // none
+          GestureDetector(
+            onTap: () => setState(() => _currentMode = MODE_NONE),
+            child: Container(
+              decoration: BoxDecoration(
+                  color:
+                  _currentMode == MODE_NONE ? Colors.black : Colors.white,
+                  borderRadius: BorderRadius.circular(6),
+                  border: Border.all(color: Colors.black)),
+              padding: const EdgeInsets.all(4),
+              child: Icon(
+                Icons.clear,
+                color: _currentMode == MODE_NONE ? Colors.white : Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _naverMap() {
+    return Expanded(
+      child: Stack(
+        children: <Widget>[
+          NaverMap(
+            onMapCreated: _onMapCreated,
+            onMapTap: _onMapTap,
+            markers: _markers,
+            initLocationTrackingMode: LocationTrackingMode.Follow,
+          ),
+        ],
+      ),
+    );
+  }
+
+  // ================== method ==========================
+
   void _onMapCreated(NaverMapController controller) {
-    if (_controller.isCompleted) _controller = Completer();
     _controller.complete(controller);
+  }
+
+  //좌표 계산 식 https://ko.wikipedia.org/wiki/%EC%A7%80%EB%A6%AC%EC%A2%8C%ED%91%9C_%EA%B1%B0%EB%A6%AC
+  //좌표 <-> 도분초 https://injunech.tistory.com/294
+  void _onMapTap(LatLng latLng) async {
+    var deviceLocation = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    debugPrint('경도 : ${latLng.longitude.toString()}');
+    debugPrint('위도 : ${latLng.latitude.toString()}');
+
+    var deviceLongitudeDegree = (deviceLocation.longitude).toInt();
+    var deviceLongitudeMinute = ((deviceLocation.longitude - deviceLongitudeDegree) * 60).toInt();
+    var deviceLongitudeSecond = ((deviceLocation.longitude - deviceLongitudeDegree) * 60 - deviceLongitudeMinute) * 60;
+
+    var deviceLatitudeDegree = (deviceLocation.latitude).toInt();
+    var deviceLatitudeMinute = ((deviceLocation.latitude - deviceLatitudeDegree) * 60).toInt();
+    var deviceLatitudeSecond = ((deviceLocation.latitude - deviceLatitudeDegree) * 60 - deviceLatitudeMinute) * 60;
+
+    var destinationLongitudeDegree = (latLng.longitude).toInt();
+    var destinationLongitudeMinute = ((latLng.longitude - destinationLongitudeDegree) * 60).toInt();
+    var destinationLongitudeSecond = ((latLng.longitude - destinationLongitudeDegree) * 60 - destinationLongitudeMinute) * 60;
+
+    var destinationLatitudeDegree = (latLng.latitude).toInt();
+    var destinationLatitudeMinute = ((latLng.latitude - destinationLatitudeDegree) * 60).toInt();
+    var destinationLatitudeSecond = ((latLng.latitude - destinationLatitudeDegree) * 60 - destinationLatitudeMinute) * 60;
+
+    var longitude = pow(((deviceLongitudeDegree - destinationLongitudeDegree).abs() * 88.9036 + (deviceLongitudeMinute - destinationLongitudeMinute).abs() * 1.4817 + (deviceLongitudeSecond - destinationLongitudeSecond).abs() * 0.0246), 2);
+    var latitude = pow(((deviceLatitudeDegree - destinationLatitudeDegree).abs() * 111.3194 + (deviceLatitudeMinute - destinationLatitudeMinute).abs() * 1.8553 + (deviceLatitudeSecond - destinationLatitudeSecond).abs() * 0.0309), 2);
+    var distance = sqrt(longitude + latitude);
+
+    debugPrint(distance.toString());
+
+    if (_currentMode == MODE_ADD) {
+      if(_markers.isEmpty){
+        _markers.add(Marker(
+          markerId: DateTime.now().toIso8601String(),
+          position: latLng,
+          infoWindow: '테스트',
+          onMarkerTab: _onMarkerTap,
+        ));
+      }else if(_markers.length == 1) {
+        _markers[0] = Marker(
+          markerId: DateTime.now().toIso8601String(),
+          position: latLng,
+          infoWindow: '테스트',
+          onMarkerTab: _onMarkerTap,
+        );
+      }
+      setState(() {});
+    }
+  }
+
+  void _onMarkerTap(Marker? marker, Map<String, int?> iconSize) {
+    int pos = _markers.indexWhere((m) => m.markerId == marker!.markerId);
+    setState(() {
+      _markers[pos].captionText = '선택됨';
+    });
+    if (_currentMode == MODE_REMOVE) {
+      setState(() {
+        _markers.removeWhere((m) => m.markerId == marker!.markerId);
+      });
+    }
   }
 
 }
