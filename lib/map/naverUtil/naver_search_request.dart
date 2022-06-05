02bd93730a073f67fb38ba521f769b192afc60fe
tranzal <64002448+tranzal@users.noreply.github.com>
2022-06-06 02:25:52 +0900

    네이버 API 좌표로 도로명 주소 가져오기

---
+++ lib/map/naverUtil/naver_search_request.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'package:naver_map_plugin/naver_map_plugin.dart';
+
+class NaverSearchRequest {
+  final addressSearchUrl = 'https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode';
+  final addressChangeUrl = 'https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc';
+  final clientId = 'ClientId';
+  final clientSecretKey = 'SecretKey';
+
+  Future<dynamic> searchPostRequest(String name, LatLng search) async {
+   var addressChangeResponse = await http.get(
+     Uri.parse('$addressChangeUrl?request=coordsToaddr&coords=${search.longitude},${search.latitude}&orders=roadaddr&output=json&sourcecrs=epsg:4326',),
+     // Uri.parse('$addressChangeUrl?request=coordsToaddr&coords=127.1234308,37.3850143&orders=roadaddr&output=json&sourcecrs=epsg:4326',),
+     headers: <String, String> {
+       'X-NCP-APIGW-API-KEY-ID' : clientId,
+       'X-NCP-APIGW-API-KEY' : clientSecretKey
+     },
+   );
+   var changeBodyList = jsonDecode(addressChangeResponse.body);
+
+    var addressSearchResponse = await http.get(
+      // Uri.parse('$url?query=$name&coordinate=${search.longitude},${search.latitude}',),
+      Uri.parse('$addressSearchUrl?query=${changeBodyList['results'][0]['region']['area1']['name'].toString()} ${changeBodyList['results'][0]['region']['area2']['name'].toString()} ${changeBodyList['results'][0]['land']['name'].toString()} ${changeBodyList['results'][0]['land']['number1'].toString()}',),
+      headers: <String, String> {
+        'X-NCP-APIGW-API-KEY-ID' : clientId,
+        'X-NCP-APIGW-API-KEY' : clientSecretKey
+      },
+    );
+    var searchBodyList = jsonDecode(addressSearchResponse.body);
+    debugPrint(searchBodyList.toString());
+    // debugPrint(searchBodyList['status']);
+    // debugPrint(searchBodyList['meta'].toString());
+    // debugPrint(searchBodyList['addresses'].toString());
+    return searchBodyList;
+  }
+}
