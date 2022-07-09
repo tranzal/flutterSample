f2ccedcca36e5dca270596ae4654d70ac37d21f5
tranzal <64002448+tranzal@users.noreply.github.com>
2022-07-09 15:02:55 +0900

    뒤로가기 두번 누르기 코드 작성 완료

--- lib/backButton/back_double_button_main.dart
+++ lib/backButton/back_double_button_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -29,13 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  DateTime? currentBackPressTime;
 
   @override
   Widget build(BuildContext context) {
@@ -43,25 +38,38 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: WillPopScope(
+        onWillPop: () async {
+          var result = onWillPop();
+          return await Future.value(result);
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+
+  bool onWillPop() {
+    var now = DateTime.now();
+    if (currentBackPressTime == null ||
+        now.difference(currentBackPressTime!) > const Duration(seconds: 2)) {
+      currentBackPressTime = now;
+      Fluttertoast.showToast(
+          msg: '뒤로가기 버튼을 한번 더 누르시면 종료됩니다.',
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: const Color(0xff6E6E6E),
+          fontSize: 15,
+          toastLength: Toast.LENGTH_SHORT);
+      return false;
+    }
+    return true;
   }
 }
