638bcde4c5bf344074ea143729e162ab8d7f9dff
jinhyeok <64002448+tranzal@users.noreply.github.com>
2022-07-26 17:09:07 +0900

    google_analytics 사용 관련 정리

--- lib/map/naverUtil/naver_search_request.dart
+++ lib/map/naverUtil/naver_search_request.dart
@@ -36,4 +36,32 @@ class NaverSearchRequest {
     // debugPrint(searchBodyList['addresses'].toString());
     return searchBodyList;
   }
+
+  Future<String> searchGetRequest(String name, LatLng search) async {
+    var addressChangeResponse = await http.post(
+      Uri.parse('$addressChangeUrl?request=coordsToaddr&coords=${search.longitude},${search.latitude}&orders=roadaddr&output=json&sourcecrs=epsg:4326',),
+      // Uri.parse('$addressChangeUrl?request=coordsToaddr&coords=127.1234308,37.3850143&orders=roadaddr&output=json&sourcecrs=epsg:4326',),
+      headers: <String, String> {
+        'X-NCP-APIGW-API-KEY-ID' : clientId,
+        'X-NCP-APIGW-API-KEY' : clientSecretKey
+      },
+    );
+    var changeBodyList = jsonDecode(addressChangeResponse.body);
+
+    var addressSearchResponse = await http.post(
+      // Uri.parse('$url?query=$name&coordinate=${search.longitude},${search.latitude}',),
+      Uri.parse('$addressSearchUrl?query=${changeBodyList['results'][0]['region']['area1']['name'].toString()} ${changeBodyList['results'][0]['region']['area2']['name'].toString()} ${changeBodyList['results'][0]['land']['name'].toString()} ${changeBodyList['results'][0]['land']['number1'].toString()}',),
+      headers: <String, String> {
+        'X-NCP-APIGW-API-KEY-ID' : clientId,
+        'X-NCP-APIGW-API-KEY' : clientSecretKey
+      },
+    );
+
+    var searchBodyList = jsonDecode(addressSearchResponse.body);
+    debugPrint(searchBodyList.toString());
+    // debugPrint(searchBodyList['status']);
+    // debugPrint(searchBodyList['meta'].toString());
+    // debugPrint(searchBodyList['addresses'].toString());
+    return searchBodyList;
+  }
 }
